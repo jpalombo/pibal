e61d8a3be964bef75420a31a3c723addb95858a6
John Palombo <johnp@palombo.org.uk>
2016-11-12 20:53:52 +0000

    interpret propeller return string

--- pibal.go
+++ pibal.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"time"
 
 	"github.com/hybridgroup/gobot"
 )
@@ -12,12 +14,23 @@ func main() {
 	propeller := NewSerialAdaptor("propeller", "/dev/ttyAMA0")
 	motor := NewMotorDriver(propeller, "motor")
 	gbot.On(propeller.Event(Data), func(data interface{}) {
+		inbytes := data.([]byte)
+		if inbytes[0] == '>' {
+		}
+		if bytes.Equal(inbytes[0:3], []byte{'+', 'g', 's'}) {
+			propeller.SerialWrite("+gs")
+		}
 		log.Printf("%q", data)
 	})
 
 	work := func() {
+		propeller.SerialWrite("+ss 0 1000")
+		propeller.SerialWrite("+ss 1 1000")
+		propeller.SerialWrite("+ss 2 1000")
+		propeller.SerialWrite("+ss 3 1000")
 		propeller.SerialWrite("+gs")
-		propeller.SerialWrite("+gp")
+		time.Sleep(2 * time.Second)
+		propeller.SerialWrite("+s")
 	}
 
 	robot := gobot.NewRobot("PiBal",
